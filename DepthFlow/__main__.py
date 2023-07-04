27f8afce6f183a260280c72753a02c60d6a49608
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2023-07-03 21:54:18 -0400

    Change the default stock render image

--- DepthFlow/__main__.py
+++ DepthFlow/__main__.py
@@ -10,7 +10,7 @@ def main():
 
     # Input Path or URL
     # depthflow.input(image=DEPTHFLOW_DIRECTORIES.DATA/"input.png")
-    depthflow.input(image="https://w.wallhaven.cc/full/p9/wallhaven-p9jrqj.png")
+    depthflow.input(image="https://w.wallhaven.cc/full/x6/wallhaven-x61e1l.jpg")
 
     # Render to video
     depthflow.render_video(
